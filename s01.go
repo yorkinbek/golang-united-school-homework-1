7411064997326ca2c008ffdc6b0524e7d2fb5a72
yorkinbek <baqoyevy@gmail.com>
2023-02-16 10:50:07 +0500

    Implement method and function

--- s01.go
+++ s01.go
@@ -1,7 +1,36 @@
 package structs
 
+type User struct {
+	firstName string
+	lastName  string
+}
+
 type UserInterface interface {
 	SetFirstName(string)
 	SetLastName(string)
 	FullName() string
+}
+
+func (user User) SetFirstName(string) {
+
+}
+
+func (user User) SetLastName(string) {
+
+}
+
+func (user User) FullName() string {
+	return ""
+}
+
+func ResetUser(input User) {
+
+}
+
+func IsUser(input User) bool {
+	return true
+}
+
+func ProcessUser(input UserInterface) string {
+	return ""
 }
