7cd2370f04605260f92c7fc2816c3f16048ff498
yorkinbek <baqoyevy@gmail.com>
2023-02-26 13:10:15 +0500

    writing full code for the first test

--- hard.go
+++ hard.go
@@ -1,24 +1,36 @@
 package hard
 
-import "strings"
+import (
+	"io"
+	"strings"
+)
 
-/*
-You own a Goal Parser that can interpret a string command.
-The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
-The Goal Parser will interpret
-"G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
-The interpreted strings are then concatenated in the original order.
-
-For example:
-input: "G()(al)
-output: Goal
-
-input: G()()()()(al)
-output: Gooooal
-
-input: (al)G(al)()()G
-output: alGalooG
-*/
-func GoalParsers(strReader *strings.Reader) string {
-	panic("Not implemented yet...")
+func GoalParser(strReader *strings.Reader) string {
+	var result string
+	for {
+		r, _, err := strReader.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		switch r {
+		case 'G':
+			result += "G"
+		case '(':
+			r, _, err := strReader.ReadRune()
+			if err == io.EOF {
+				break
+			}
+			if r == ')' {
+				result += "o"
+			} else {
+				strReader.UnreadRune()
+				str, _, _ := strReader.ReadRune()
+				str, _, _ = strReader.ReadRune()
+				if str == 'l' {
+					result += "al"
+				}
+			}
+		}
+	}
+	return result
 }
